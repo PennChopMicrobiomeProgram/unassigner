34082c03e288b3419e0adc6d8ea9da9c9274fb54
Kyle Bittinger <kylebittinger@gmail.com>
2015-01-07 11:51:57 -0500

    Simplified function hit_identity and added tests.

--- tests/test_search_blast.py
+++ tests/test_search_blast.py
@@ -12,9 +12,9 @@ class FuntionTests(unittest.TestCase):
             "qseq": "CCCGGTCCGGTTATT",
             "sseq": "CCCGGTCCGGTTAAC",
             "qstart": 1, "qend": 15, "qlen": 15,
-            "sstart": 1, "send": 15, "qlen": 15,
+            "sstart": 1, "send": 15, "slen": 15,
             }
-        self.assertEqual(hit_identity(hit), 13 / 15)
+        self.assertEqual(hit_identity(hit), (13, 15))
 
     def test_hit_identity_equal_endgaps_left(self):
         # Hit has 15 positions and 2 mismatches (rightmost columns).
@@ -24,9 +24,9 @@ class FuntionTests(unittest.TestCase):
             "qseq": "CCCGGTCCGGTTATT",
             "sseq": "CCCGGTCCGGTTAAC",
             "qstart": 6, "qend": 20, "qlen": 20,
-            "sstart": 6, "send": 20, "qlen": 20,
+            "sstart": 6, "send": 20, "slen": 20,
             }
-        self.assertEqual(hit_identity(hit), 13 / 20)
+        self.assertEqual(hit_identity(hit), (13, 20))
 
     def test_hit_identity_equal_endgaps_right(self):
         # Hit has 15 positions and 2 mismatches (rightmost columns).
@@ -36,9 +36,87 @@ class FuntionTests(unittest.TestCase):
             "qseq": "CCCGGTCCGGTTATT",
             "sseq": "CCCGGTCCGGTTAAC",
             "qstart": 1, "qend": 15, "qlen": 20,
-            "sstart": 1, "send": 15, "qlen": 20,
+            "sstart": 1, "send": 15, "slen": 20,
             }
-        self.assertEqual(hit_identity(hit), 13 / 20)
+        self.assertEqual(hit_identity(hit), (13, 20))
+
+    def test_hit_identity_query_endgaps_left(self):
+        # Hit has 15 positions and 2 mismatches (rightmost columns).
+        # Query has 3 unaligned bases to the left of the local
+        # alignment region.
+        hit = {
+            "qseq": "CCCGGTCCGGTTATT",
+            "sseq": "CCCGGTCCGGTTAAC",
+            "qstart": 4, "qend": 18, "qlen": 18,
+            "sstart": 6, "send": 20, "slen": 20,
+            }
+        self.assertEqual(hit_identity(hit), (13, 18))
+
+    def test_hit_identity_query_endgaps_right(self):
+        # Hit has 15 positions and 2 mismatches (rightmost columns).
+        # Query has 3 unaligned bases to the right of the local
+        # alignment region.
+        hit = {
+            "qseq": "CCCGGTCCGGTTATT",
+            "sseq": "CCCGGTCCGGTTAAC",
+            "qstart": 1, "qend": 15, "qlen": 18,
+            "sstart": 1, "send": 15, "slen": 20,
+            }
+        self.assertEqual(hit_identity(hit), (13, 18))
+
+    def test_hit_identity_subject_endgaps_left(self):
+        # Hit has 15 positions and 2 mismatches (rightmost columns).
+        # Subject has 3 unaligned bases to the left of the local
+        # alignment region.
+        hit = {
+            "qseq": "CCCGGTCCGGTTATT",
+            "sseq": "CCCGGTCCGGTTAAC",
+            "qstart": 6, "qend": 20, "qlen": 20,
+            "sstart": 4, "send": 18, "slen": 18,
+            }
+        self.assertEqual(hit_identity(hit), (13, 18))
+
+    def test_hit_identity_subject_endgaps_right(self):
+        # Hit has 15 positions and 2 mismatches (rightmost columns).
+        # Query has 3 unaligned bases to the right of the local
+        # alignment region.
+        hit = {
+            "qseq": "CCCGGTCCGGTTATT",
+            "sseq": "CCCGGTCCGGTTAAC",
+            "qstart": 1, "qend": 15, "qlen": 20,
+            "sstart": 1, "send": 15, "slen": 18,
+            }
+        self.assertEqual(hit_identity(hit), (13, 18))
+
+    def test_hit_identity_query_gaps(self):
+        # Hit has 15 positions and 3 mismatches (rightmost columns).
+        # Query has 1 nt to right of the alignment.
+        # Total matches: 15 - 3 = 12
+        # Total positions counted: 15 + 1 = 16
+        hit = {
+            "qseq": "CCCGGTCCGGTT-TT",
+            "sseq": "CCCGGTCCGGTTAAC",
+            "qstart": 1, "qend": 14, "qlen": 15,
+            "sstart": 1, "send": 15, "slen": 20,
+            }
+        self.assertEqual(hit_identity(hit), (12, 16))
+
+    def test_hit_identity_region(self):
+        # Hit has 15 positions and 3 mismatches (rightmost columns).
+        # Query has 1 nt to right of the alignment.
+        # Specified region begins in position 5 of query (first 4
+        #   positions of query ignored).
+        # Total matches: 15 - 4 - 3 = 8
+        # Total positions counted: 15 - 4 + 1 = 12
+        hit = {
+            "qseq": "CCCGGTCCGGTT-TT",
+            "sseq": "CCCGGTCCGGTTAAC",
+            "qstart": 1, "qend": 14, "qlen": 15,
+            "sstart": 1, "send": 15, "slen": 20,
+            }
+        
+        self.assertEqual(hit_identity(hit, 5, 15), (8, 12))
+
 
 if __name__ == "__main__":
     unittest.main()
--- unassign/search_blast.py
+++ unassign/search_blast.py
@@ -66,40 +66,79 @@ def hit_identity(hit):
     return matches / total
 
 
-def hit_region_identity(hit, qstart=0, qend=0):
-    in_total = 0
-    in_matches = 0
+def hit_identity(hit, start=None, end=None):
+    """Count regional and total matches in BLAST hit.
+
+    Parameters
+    ----------
+    hit : a dictionary representing the BLAST hit, must have the
+        following keys: qseq, sseq, qstart, qend, qlen
+    start : start position in query sequence
+    end : end position in query sequence
+
+    Returns
+    -------
+    tuple containing two ints
+        Number of matching positions and number of nucleotides in query.
+
+    Notes
+    -----
+    Because sequence positions are indexed from 1 in BLAST, the
+    start and end positions are indexed starting from 1.
+    """
+    if start is None:
+        start = 1
+    if end is None:
+        end = hit['qlen']
 
     total = 0
     matches = 0
-    
-    # matches in alignment region
+
+    # Count matches in alignment region.
     qpos = hit['qstart']
     for qchar, hchar in zip(hit['qseq'], hit['sseq']):
-        if qstart <= qpos <= qend:
-            in_total += 1
+        if start <= qpos <= end:
+            total += 1
             if qchar == hchar:
-                in_matches += 1
-        total += 1
-        if qchar == hchar:
-            matches += 1
+                matches += 1
         if qchar != '-':
             qpos += 1
 
-    # if query is not aligned at first base
-    if hit['qstart'] > 1:
-        # Count unaligned nts as part of total
-        total = total + hit['qstart'] - 1
-        
-        # TODO: check if we are at the end of subject sequence and do
-        # not count if subject can't be extended.
+    # If query is not aligned at the start position, count positions
+    # from the start position to the left of the alignment as
+    # mismatches.
+    if start < hit['qstart']:
+        # Number of nts in query from left end of alignment to query
+        # start position.
+        query_left = hit['qstart'] - start
 
-    # if query is not aligned at last base
-    if hit['qend'] < hit['qlen']:
-        # Count unaligned nts as part of total
-        total = total + hit['qlen'] - hit['qend']
+        # Want to make sure there are at least this many nts available
+        # for alignment in the subject sequence.  Number of nts in
+        # subject to left of alignment.
+        subject_left = hit['sstart'] - 1
 
-        # TODO: again, check if we are out of range in the subject seqs
+        # Add the minimum of query_left vs. subject_left to the total.
+        if query_left < subject_left:
+            total += query_left
+        else:
+            total += subject_left
+
+    # If query is not aligned at the end position, count positions to the
+    # right of the alignment as mismatches.
+    if end > hit['qend']:
+        # Number of nts in query from right end of alignment to query
+        # end position.
+        query_right = end - hit['qend']
+
+        # Number of nts in subject to right of alignment.
+        subject_right = hit['slen'] - hit['send']
+
+        # Again, add the smaller number to the total.
+        if query_right < subject_right:
+            total += query_right
+        else:
+            total += subject_right
+
         
-    return in_matches, in_total, matches, total
+    return matches, total
 
