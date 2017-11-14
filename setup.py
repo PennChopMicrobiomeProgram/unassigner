dcaf3789804acada5dc4f8ce304dca2b881c17a8
Kyle Bittinger <kylebittinger@gmail.com>
2017-11-14 13:07:14 -0500

    Reformat setup file

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
 
-setup(name='unassign',
-      version='0.0.1',
-      description='Bacterial species unassigner',
-      author='Kyle Bittinger',
-      author_email='kylebittinger@gmail.com',
-      url='https://github.com/kylebittinger/unassigner',
-      packages=['unassign'],
-      entry_points = {
-          'console_scripts': [
-              'unassignseqs=unassign.command:main',
-              'prepare_strain_data=unassign.prepare_strain_data:main',
-          ],
-      }
+setup(
+    name='unassign',
+    version='0.0.1',
+    description='Bacterial species unassigner',
+    author='Kyle Bittinger',
+    author_email='kylebittinger@gmail.com',
+    url='https://github.com/kylebittinger/unassigner',
+    packages=['unassign'],
+    entry_points = {
+        'console_scripts': [
+            'unassignseqs=unassign.command:main',
+            'prepare_strain_data=unassign.prepare_strain_data:main',
+            'trimragged=unassign.trimragged:main',
+        ],
+    }
 )
