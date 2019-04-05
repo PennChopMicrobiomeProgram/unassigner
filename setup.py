cb389c8261bb5812cce4863948f81ee2ea8ba8c6
Kyle Bittinger <kylebittinger@gmail.com>
2019-04-05 10:58:07 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='unassign',
-      version='0.0.1',
+      version='0.0.2',
       description='Bacterial species unassigner',
       author='Kyle Bittinger',
       author_email='kylebittinger@gmail.com',
