14b0f4af166182d674f457f1381ab4f91454a4a0
ctbus <ctbushman@gmail.com>
2023-02-08 14:01:48 -0500

    Only test parse_desc

--- tests/test_download.py
+++ tests/test_download.py
@@ -10,17 +10,17 @@ class DownloadTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_parsers(self):
-        inputs = (
-            ">lcl\\|LTP_06_2022\\|TraGuam2 [organism=Organism name] [strain=NBRC 103172 s[T]] [accession=AC123456]\n"
-            "AUUGAACGCUGGCGGCAGGCCUAACACAUGCAAGUCGAGCGGCAGCGGGGGAA\n"
-            ">lcl\\|LTP_06_2022\\|TraOdont [organism=Organism name] [strain=Eant 3-9 l[T] r[T] s[T]] [accession=AC123456]\n"
-            "AUGCAAGUCGAGCGGCAGCGGGGGAAAGCUUGCUUUCCCGCCGGCGAGCGGCGG\n"
-            ">lcl\\|LTP_06_2022\\|EnbTribo [organism=Organism name] [strain=IG-V01 l[T]] [accession=AC123456]\n"
-            "UCCAGAGUUUGAUCAUGGCUCAGAUUGAACGCUGGCGGCAGGCCUAACACAUGC\n"
-        )
-
-        for desc, seq in parse_fasta(inputs):
+    def test_parse_desc(self):
+        seqs = {
+            ">lcl\\|LTP_06_2022\\|TraGuam2 [organism=Organism name] [strain=NBRC 103172 s[T]] [accession=AC123456]\n":
+            "AUUGAACGCUGGCGGCAGGCCUAACACAUGCAAGUCGAGCGGCAGCGGGGGAA\n",
+            ">lcl\\|LTP_06_2022\\|TraOdont [organism=Organism name] [strain=Eant 3-9 l[T] r[T] s[T]] [accession=AC123456]\n":
+            "AUGCAAGUCGAGCGGCAGCGGGGGAAAGCUUGCUUUCCCGCCGGCGAGCGGCGG\n",
+            ">lcl\\|LTP_06_2022\\|EnbTribo [organism=Organism name] [strain=IG-V01 l[T]] [accession=AC123456]\n":
+            "UCCAGAGUUUGAUCAUGGCUCAGAUUGAACGCUGGCGGCAGGCCUAACACAUGC\n",
+        }
+            # needs a file handler
+        for desc, seq in seqs:
             print(desc)
             self.assertEqual(desc[0], ">")
             self.assertEqual(set(list(seq)), set("A", "C", "G", "U"))
